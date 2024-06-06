83a128d652c5eeaa4a326a45b9a601c2dd8aeab8
Julian Kim <kimjulian@grinnell.edu>
2024-06-06 10:56:14 -0500

    Added textboxes disabling tests

--- frontend/src/converter/Converter.spec.tsx
+++ frontend/src/converter/Converter.spec.tsx
@@ -1,7 +1,7 @@
 import Converter from "./Converter.tsx";
 import {render, screen, waitFor} from "@testing-library/react";
 import {userEvent} from "@testing-library/user-event";
-import {afterEach, beforeAll} from "vitest";
+import {afterEach, beforeAll, expect} from "vitest";
 import {server} from "../testing/mockBackend.ts";
 import {http, HttpResponse} from "msw";
 
@@ -61,5 +61,42 @@ describe('Converter Component', () => {
         await waitFor(() => {expect(expectedElement).toBeEnabled()})
 
         expect(expectedElement).toHaveValue("II")
+    })
+
+    test('textboxes disable after click convert', async () => {
+        server.use(http.post("http://localhost:5000/convert/romanToInteger", () => {
+            return HttpResponse.json();
+        }));
+        const user = userEvent.setup();
+
+        render(<Converter />);
+
+        user.click(screen.getByRole("button")).then(async () => {
+            expect(screen.getByRole("textbox", { name: "Roman numeral" })).toBeDisabled();
+            expect(screen.getByRole("textbox", { name: "Integer" })).toBeDisabled();
+        })
+    })
+
+    test('textboxes re-enable after server response', async () => {
+        server.use(http.post("http://localhost:5000/convert/romanToInteger", () => {
+            return HttpResponse.json({
+                roman: "II",
+                integer: 2
+            });
+        }));
+        const user = userEvent.setup();
+
+        render(<Converter />);
+
+        await user.click(screen.getByRole("button"));
+
+        const romanNumeralTextbox = screen.getByRole("textbox", { name: "Roman numeral" });
+
+        await waitFor(() => {
+            expect(romanNumeralTextbox).toHaveValue("II");
+        })
+
+        expect(romanNumeralTextbox).toBeEnabled();
+        expect(screen.getByRole("textbox", { name: "Integer" })).toBeEnabled();
     })
 })
