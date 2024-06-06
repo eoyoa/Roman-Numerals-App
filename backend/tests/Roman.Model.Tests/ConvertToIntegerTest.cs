6dfd2164f29a9978dc9b8eaeb9b9772af7fac1d4
khanhdo05 <khanhphuongdo28@gmail.com>
2024-06-05 23:15:32 -0500

    Extract method for assertion equality function and add comments for documentation

--- backend/tests/Roman.Model.Tests/ConvertToIntegerTest.cs
+++ backend/tests/Roman.Model.Tests/ConvertToIntegerTest.cs
@@ -1,7 +1,24 @@
 namespace Roman.Model.Tests;
-
+/*
+ * This test file makes sure that the calculation/conversion from
+ *  Roman symbol (string), to its equivalent integer (int) is correct.
+ */
 public class ConvertToIntegerTest
 {
+    // Test helper method that test whether the function ConvertToInteger when takes in a symbol (string)
+    //  returns the expected/correct value (integer) or not.
+    private void AssertConversion(int expectedValue, string symbol)
+    {
+        Assert.Equal(expectedValue, RomanNumerals.ConvertToInteger(symbol));
+    }
+    
+    /*
+     * Test Batch 1: When Roman numeral is one of the unique character
+     *   - These tests make sure that the conversion between Roman numeral of 1 character
+     *   to its equivalent integer is correct.
+     *   - Testing the easiest case where the symbol maps to a value in the structure containing
+     *   all the single and double symbols numbers.
+     */
     [Theory]
     [InlineData(1, "I")]
     [InlineData(5, "V")]
@@ -12,18 +29,32 @@ public class ConvertToIntegerTest
     [InlineData(1000, "M")]
     public void TestSingleSymbolNumbers(int expectedValue, string symbol)
     {
-        Assert.Equal(expectedValue, RomanNumerals.ConvertToInteger(symbol));
+        AssertConversion(expectedValue, symbol);
     }
 
+    /*
+     * Test Batch 2: When Roman numeral has multiple digits of the same character
+     *   - These tests make sure that the conversion between Roman numeral that has multiple digits
+     *   of one character and its equivalent integer is correct.
+     *   - Testing that when two adjacent characters maps to the same integer value, calculation is
+     *   still correct.
+     */
     [Theory]
     [InlineData(2, "II")]
     [InlineData(3, "III")]
     [InlineData(20, "XX")]
     public void TestSameDigits(int expectedValue, string symbol)
     {
-        Assert.Equal(expectedValue, RomanNumerals.ConvertToInteger(symbol));
+        AssertConversion(expectedValue, symbol);
     }
     
+    /*
+     * Test Batch 3: When Roman numeral has different character digits
+     *   - These tests make sure that the conversion between Roman numeral that has multiple digits
+     *   of one character and its equivalent integer is correct.
+     *   - Testing that when two adjacent characters don't map to the same integer value, calculation
+     *   is still correct.
+     */
     [Theory]
     [InlineData(6, "VI")]
     [InlineData(7, "VII")]
@@ -31,9 +62,16 @@ public class ConvertToIntegerTest
     [InlineData(1006, "MVI")]
     public void TestDifferentDigits(int expectedValue, string symbol)
     {
-        Assert.Equal(expectedValue, RomanNumerals.ConvertToInteger(symbol));
+        AssertConversion(expectedValue, symbol);
     }
     
+    /*
+     * Test Batch 4: When Roman numeral maps to integer in the prefixed structure 
+     *   - These tests make sure that the conversion between Roman numeral that has more than 1 digits
+     *   but is defined in the prefixed structure.
+     *   - Testing that when two adjacent characters maps to the same integer value, calculation is
+     *   still correct.
+     */
     [Theory]
     [InlineData(4, "IV")]
     [InlineData(90, "XC")]
@@ -41,9 +79,14 @@ public class ConvertToIntegerTest
     [InlineData(900, "CM")]
     public void TestPrefixedDigits(int expectedValue, string symbol)
     {
-        Assert.Equal(expectedValue, RomanNumerals.ConvertToInteger(symbol));
+        AssertConversion(expectedValue, symbol);
     }
     
+    /*
+     * Test Batch 5: When Roman numeral could be anything randomly generated in range 1-1000 
+     *   - These tests make sure that the conversion between any Roman numeral and its equivalent
+     *   integer is correct.
+     */
     [Theory]
     [InlineData(857, "DCCCLVII")]
     [InlineData(484, "CDLXXXIV")]
@@ -53,7 +96,7 @@ public class ConvertToIntegerTest
     [InlineData(495, "CDXCV")]
     public void TestRandomNumberGenerator(int expectedValue, string symbol)
     {
-        Assert.Equal(expectedValue, RomanNumerals.ConvertToInteger(symbol));
+        AssertConversion(expectedValue, symbol);
     }
     
 }
